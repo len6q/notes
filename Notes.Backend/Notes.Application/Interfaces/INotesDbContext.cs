1d6bdc2aabf430c2f61bffcc75242b74232e8e87
len6q <101547536+len6q@users.noreply.github.com>
2023-02-27 16:47:39 +0600

    Add database in project

---
+++ Notes.Backend/Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+using Notes.Domain;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        
+        Task<int> SaveChangesAsync(CancellationChangeToken cancellationChangeToken);
+    }
+}
---
+++ Notes.Backend/Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+using System;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set ; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public Task<int> SaveChangesAsync(CancellationChangeToken cancellationChangeToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
