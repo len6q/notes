a88d424fc2d46a660c0fbff0977966e66364c292
len6q <101547536+len6q@users.noreply.github.com>
2023-02-27 17:04:26 +0600

    Add automapper

--- Notes.Backend/Notes.Persistence/NotesDbContext.cs
+++ Notes.Backend/Notes.Persistence/NotesDbContext.cs
@@ -10,7 +10,7 @@ namespace Notes.Persistence
 {
     public class NotesDbContext : DbContext, INotesDbContext
     {
-        public DbSet<Note> Notes { get; set ; }
+        public DbSet<Note> Notes { get; set; }
 
         public NotesDbContext(DbContextOptions<NotesDbContext> options)
             : base(options) { }
