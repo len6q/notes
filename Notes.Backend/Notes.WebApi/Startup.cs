f2e1be209f6553d6421fd5aa811dd89902218245
len6q <101547536+len6q@users.noreply.github.com>
2023-02-28 16:27:03 +0600

    Create WebApi

--- Notes.Backend/Notes.WebApi/Startup.cs
+++ Notes.Backend/Notes.WebApi/Startup.cs
@@ -1,24 +1,50 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Notes.Application;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Persistence;
+using System.Reflection;
 
 namespace Notes.WebApi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private const string ALLOWALL = "AllowAll";
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+            services.AddAutoMapper(config =>
+            {
+                config.AddProfile(new
+                    AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+                config.AddProfile(new
+                    AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+            });
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            services.AddAplication();
+            services.AddPersistence(Configuration);
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(ALLOWALL, policy =>
+                {
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyOrigin();
+                });
+            });
+        }
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,13 +53,12 @@ namespace Notes.WebApi
             }
 
             app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseCors(ALLOWALL);
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
