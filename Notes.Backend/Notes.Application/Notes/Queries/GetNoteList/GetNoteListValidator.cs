90c2beabfa22814d97d199a7ccba39647ba30acd
len6q <101547536+len6q@users.noreply.github.com>
2023-03-03 14:05:39 +0600

    Validation data

---
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteList/GetNoteListValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListValidator : AbstractValidator<GetNoteListQuery>
+    {
+        public GetNoteListValidator()
+        {
+            RuleFor(note => note.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
